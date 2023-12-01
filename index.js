a4a717974613b7fdb481752b801288986c9367e9
noel-pena <azulu88@hotmail.com>
2023-12-01 12:27:51 -0500

    first push

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+    try {
+        const response = await axios.get("https://programming-quotesapi.vercel.app/api/random");
+        res.render("index.ejs", { quote: result.data.quote, author: result.data.author });
+        console.log(quote)
+    } catch (error) {
+        res.status(500);
+    };
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
